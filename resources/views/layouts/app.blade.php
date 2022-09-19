8b6d0c6c6bd7286cf60ac306f9e96c6e0143cdd5
Kotov584 <85634618+Kotov584@users.noreply.github.com>
2022-09-19 08:19:46 -0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <header>
         <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#">Completed test for Eshopwithiq</a>
+            <a class="navbar-brand" href="#">Import csv</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
